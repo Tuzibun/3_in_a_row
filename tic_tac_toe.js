0f96a90440b484219dafff183be2f19bfc5298b4
GWEN <gwenmak@gmail.com>
2015-05-01 10:48:21 -0400

    initial commit

---
+++ tic_tac_toe.js
@@ -0,0 +1,21 @@
+var board_table = <table class="board" border="0px" 
+	cellpadding="0px" 
+	cellspacing="0px" 
+	align="center">
+	
+	<tr>
+	<td id="ttt0">&nbsp;</td>
+	<td id="ttt1">&nbsp;</td>
+	<td id="ttt2">&nbsp;</td>
+	</tr>
+	<tr>
+	<td id="ttt3">&nbsp;</td>
+	<td id="ttt4">&nbsp;</td>
+	<td id="ttt5">&nbsp;</td>
+	</tr>
+	<tr>
+	<td id="ttt6">&nbsp;</td>
+	<td id="ttt7">&nbsp;</td>
+	<td id="ttt8">&nbsp;</td>
+	</tr>
+	</table>;
