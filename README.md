dcb7b014927e128ed03a29cee9ea68407e2f5d49
Jose Teotonio S Neto <teonett@MacBook-Pro-de-Jose.local>
2020-10-15 22:46:35 -0300

    Game evolution

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# 3_in_a_row
-team awesome tic-tac-toe
+<h4>Copyright (C) 2020 Gwen Tuzibun - github.com/Tuzibun</h4>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+</p>
+
+<img src="https://github.com/teonett/Javascript-Jogo-da-Velha/blob/master/Captura%20de%20Tela%202020-02-08%20%C3%A0s%2011.54.29.png">
--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,30 +1,186 @@
-var one = $("#1")
-var two = $("#2")
-var three = $("#3")
-var four = $("#4")
-var five = $("#5")
-var six = $("#6")
-var seven = $("#7")
-var eight = $("#8")
-var nine = $("#9")
+let playerSymbol;
+let computerSymbol;
 
-var boardSquares = [one, two, three, four, five, six, seven, eight, nine];
-var player = 1
+const alert = document.querySelector('.alert-text');
+const gameArea = document.getElementById("gameArea");
+const gameSquare = document.querySelectorAll('.gameSquare');
 
-for (var i = 0; i < boardSquares.length, i++){
-	
-}
+const square1 = document.getElementById("gameSquare1");
+const square2 = document.getElementById("gameSquare2");
+const square3 = document.getElementById("gameSquare3");
+const square4 = document.getElementById("gameSquare4");
+const square5 = document.getElementById("gameSquare5");
+const square6 = document.getElementById("gameSquare6");
+const square7 = document.getElementById("gameSquare7");
+const square8 = document.getElementById("gameSquare8");
+const square9 = document.getElementById("gameSquare9");
 
-//square one
-one.click(function(){
-	if (player === 1){
-		one.text("^X^");
-		player = 2;
-	}
-	else {
-		one.text("^0^");
-		player = 1;
-	}
+const chooseSideBtns = document.querySelectorAll('.btn-chooseside')
+const chooseSideX = document.getElementById("chooseSideX");
+const chooseSideO = document.getElementById("chooseSideO");
+
+chooseSide = document.querySelector('.choose-side');
+playInstruction = document.querySelector('.play-instruction');
+
+playerSymbolX = `<img class="gameSquare_icon" src="./images/close.png" alt="Cross">`;
+playerSymbolO = `<img class="gameSquare_icon" src="./images/rec.png" alt="Nought">`;
+
+const endGameModal = document.getElementById('endGameModal');
+
+let winningSymbol;
+let wonGame = false;
+
+let winningText = document.querySelector('.winning-text')
+
+
+//player chooses side
+
+chooseSideBtns.forEach(function(button){
+    button.addEventListener('click', function(e){
+
+        const item = e.target;
+
+        if (item.classList.contains('chooseSideX')){
+            playerSymbol = playerSymbolX;
+            computerSymbol = playerSymbolO ;
+            
+
+        } else if (item.classList.contains('chooseSideO')){
+            playerSymbol = playerSymbolO;
+            computerSymbol = playerSymbolX;
+        } else {
+            alert("Error. Please choose a player symbol");
+        }
+
+        chooseSide.style.display = 'none';
+        playInstruction.style.display = 'block';
+        gameArea.style.pointerEvents = 'auto';
+    })
 });
 
-//square one
+
+//add symbols to grid
+
+gameSquare.forEach(function(square){
+    square.addEventListener('click', function(e){
+        item = e.target;
+        
+        if (item.innerHTML === ""){
+            item.innerHTML = playerSymbol;
+
+            gameArea.style.pointerEvents = 'none';
+
+            playerHasWon();
+
+            if (wonGame == false){
+                setTimeout(randomComputerSquare, 650);  
+                setTimeout(function(){
+                gameArea.style.pointerEvents = 'auto';
+                }, 650);
+            }
+
+        } else {
+            alert.style.display = 'block';
+            setTimeout(function(){
+                alert.style.display = 'none';
+                }, 1700);
+        }
+    })
+})
+
+
+//function to randomly select computer square
+
+function randomComputerSquare(){
+
+    const randomNumber = Math.floor((Math.random()*9)+1);
+    const squareNumber = document.getElementById("gameSquare" + randomNumber);
+    
+    if (squareNumber.innerHTML == "") {
+        squareNumber.innerHTML = computerSymbol;
+
+        playerHasWon();
+
+    } else if (squareNumber.innerHTML !== ""){
+        randomComputerSquare();
+    };
+
+    
+};
+
+//check if game has been won
+
+function playerHasWon(){
+
+        if (square1.innerHTML === playerSymbolX && square2.innerHTML === playerSymbolX && square3.innerHTML === playerSymbolX || 
+            square4.innerHTML === playerSymbolX && square5.innerHTML === playerSymbolX && square6.innerHTML === playerSymbolX || 
+            square7.innerHTML === playerSymbolX && square8.innerHTML === playerSymbolX && square9.innerHTML === playerSymbolX || 
+            square1.innerHTML === playerSymbolX && square4.innerHTML === playerSymbolX && square7.innerHTML === playerSymbolX || 
+            square2.innerHTML === playerSymbolX && square5.innerHTML === playerSymbolX && square8.innerHTML === playerSymbolX || 
+            square3.innerHTML === playerSymbolX && square6.innerHTML === playerSymbolX && square9.innerHTML === playerSymbolX || 
+            square1.innerHTML === playerSymbolX && square5.innerHTML === playerSymbolX && square9.innerHTML === playerSymbolX || 
+            square3.innerHTML === playerSymbolX && square5.innerHTML === playerSymbolX && square7.innerHTML === playerSymbolX){
+          
+            winningSymbol = `<img class="gameSquare_icon" src="./images/close.png" alt="Cross">`;
+            wonGame = true;
+
+            endGame();
+            winText();
+        } else if (
+            square1.innerHTML === playerSymbolO && square2.innerHTML === playerSymbolO && square3.innerHTML === playerSymbolO || 
+            square4.innerHTML === playerSymbolO && square5.innerHTML === playerSymbolO && square6.innerHTML === playerSymbolO || 
+            square7.innerHTML === playerSymbolO && square8.innerHTML === playerSymbolO && square9.innerHTML === playerSymbolO || 
+            square1.innerHTML === playerSymbolO && square4.innerHTML === playerSymbolO && square7.innerHTML === playerSymbolO || 
+            square2.innerHTML === playerSymbolO && square5.innerHTML === playerSymbolO && square8.innerHTML === playerSymbolO || 
+            square3.innerHTML === playerSymbolO && square6.innerHTML === playerSymbolO && square9.innerHTML === playerSymbolO || 
+            square1.innerHTML === playerSymbolO && square5.innerHTML === playerSymbolO && square9.innerHTML === playerSymbolO || 
+            square3.innerHTML === playerSymbolO && square5.innerHTML === playerSymbolO && square7.innerHTML === playerSymbolO){
+        
+            winningSymbol = `<img class="gameSquare_icon" src="./images/rec.png" alt="Nought">`;
+            wonGame = true;
+
+            endGame();
+            winText();
+        } else {
+            
+            if (square1.innerHTML !== "" && square2.innerHTML !== "" && square3.innerHTML !== "" && square4.innerHTML !== "" && square5.innerHTML !== "" && square6.innerHTML !== "" && square7.innerHTML !== "" && square8.innerHTML !== "" && square9.innerHTML !== ""){
+                 
+                wonGame = true;
+                winningSymbol == null;
+
+                endGame();
+                winText();
+            };
+        };
+
+};
+
+function endGame(){
+    gameArea.style.pointerEvents = 'none';
+}
+
+// show winning text
+
+function winText(){
+    
+    setTimeout(function(){
+        playInstruction.style.display = "none";
+        $('#endGameModal').modal('show');
+    }, 690)
+    
+
+    if (winningSymbol == playerSymbol){
+        winningText.innerText = "Congratulations, you won the game!"
+    }else if (winningSymbol == computerSymbol){
+        winningText.innerText = "Sorry, you have not won this time!"
+    }else{
+        winningText.innerText = "No one won this time!"
+    }
+
+    const playAgain = document.getElementById("play-again");
+
+    playAgain.addEventListener('click', function(){
+        location.reload();
+    })
+
+};
